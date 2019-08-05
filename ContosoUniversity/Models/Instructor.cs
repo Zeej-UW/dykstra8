455e6fc449a866251ba54e13a04578f82345ef2f
Charles Jackson <cjjaxx@uw.edu>
2019-08-04 22:11:58 -0700

    Initial commit

---
+++ ContosoUniversity/Models/Instructor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+/*
+ * Practically the same as the Student class but for instructors instead. Only
+ * added thing is a getter/setter for two new addtl. items: OfficeAssignment and CourseAssignments.
+ */
+namespace ContosoUniversity.Models
+{
+    public class Instructor
+    {
+        public int ID { get; set; }
+
+        // Reduced the multiple lines to one—functionally the same.
+        [Required, Display(Name = "Last Name"), StringLength(50)]
+        public string LastName { get; set; }
+
+
+        [Required, Column("FirstName"), Display(Name = "First Name"), StringLength(50)]
+        public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date), Display(Name = "Hire Date"), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime HireDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get { return LastName + ", " + FirstMidName; }
+        }
+
+        // An instructor can have multiple CourseAssignments, hence the collection
+        public ICollection<CourseAssignment> CourseAssignments { get; set; }
+        
+        // An instructor can only have one office
+        public OfficeAssignment OfficeAssignment { get; set; }
+    }
+}
---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        // Similar to varchar(50) in SQL—forces a character limit on the names being entered.
+        // Allows them to enter whitespace if wanted. RegularExpression will force the user to abide by certain input.
+        // Also forces that a value be entered and changes the display name of the column within the table.
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
+        public string FirstMidName { get; set; }
+        // Makes it so our date string is formatted in a particular way.
+        // Forces the date to abide by year/month/day vs. year/month/day/time.
+        // 2nd param forces the edit box to abide by this rule and display it when viewed.
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+
+        // Creates a full name from the other two columns.
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
